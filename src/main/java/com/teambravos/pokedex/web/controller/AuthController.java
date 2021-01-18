d7dd41196c6335e6d4bae8db9d34a41726a9e2b3
Valeri Esquivel <1530205@upv.edu.mx>
2021-01-17 22:25:00 -0600

    Valeria Esquivel

--- src/main/java/com/teambravos/pokedex/web/controller/AuthController.java
+++ src/main/java/com/teambravos/pokedex/web/controller/AuthController.java
@@ -44,7 +44,6 @@ public class AuthController {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
             UserDetails userDetails = pokemonUserService.loadUserByUsername(request.getUsername());
             String jwt = jwtUtil.generateToken(userDetails);
-
             return new ResponseEntity<>(new AuthResponse(jwt), HttpStatus.OK);
         }catch(BadCredentialsException e){
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
